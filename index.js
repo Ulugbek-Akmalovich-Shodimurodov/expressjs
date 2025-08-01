8ee27a87d9b15e3cc5de8b292635416d9b5ffb50
Ulugbek-Akmalovich-Shodimurodov <99031894+Ulugbek-Akmalovich-Shodimurodov@users.noreply.github.com>
2025-08-01 18:10:28 +0500

    q
    wertyhj

--- index.js
+++ index.js
@@ -1,15 +1,20 @@
 const express = require('express')
 const app = express();
 
-app.get('/', (req, res)=>{
+app.get('/', (req, res) => {
     res.send('Salom')
 })
 
-app.get('/api/books', (req, res)=>{
-    res.send(['qwert', '12345', 'asdfg'])
+app.get('/api/books', (req, res) => {
+    res.send(['qwert', '12345', 'asdfg', 'adhudcxmas'])
 })
 
-app.listen(5000, ()=>{
-    console.log('5000chi portni eshitishni boshladim...');
-    
+app.get('/api/books', (req, res) => {
+    res.send(['qwert', '12345', 'asdfg', 'adhudcxmas'])
+})
+
+const port = process.env.PORT || 5000;
+
+app.listen(port, () => {
+    console.log(`${port}chi portni eshitishni boshladim...`);
 })
