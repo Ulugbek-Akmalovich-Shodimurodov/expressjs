98a4275f0994bb79e57c97ae2b9ca11a4ba24936
Ulugbek-Akmalovich-Shodimurodov <99031894+Ulugbek-Akmalovich-Shodimurodov@users.noreply.github.com>
2025-08-05 10:38:41 +0500

    10:38

--- index.js
+++ index.js
@@ -1,13 +1,39 @@
 const express = require('express')
 const app = express();
 
+let books = [
+    {
+        id: 1,
+        name: "O'tkan kunlar"
+    },
+    {
+        id: 2,
+        name: "Kecha va kunduz"
+    },
+    {
+        id: 3,
+        name: "Mehrobdan chayon"
+    },
+    {
+        id: 4,
+        name: "Alpomish"
+    },
+]
+
 app.get('/', (req, res) => {
     res.send('Salom')
 })
 
-app.get('/api/books', (req, res) => {
-    res.send(['qwert', '12345', 'asdfg', 'adhudcxmas'])
-})
+app.get('/api/books/:id', (req, res) => {
+    const bookId = parseInt(req.params.id);
+    const findBook = books.find(book => book.id === bookId);
+
+    if (!findBook) {
+        return res.status(404).send('Kitob topilmadi');
+    }
+
+    res.send(findBook);
+});
 
 app.get('/api/books', (req, res) => {
     res.send(['qwert', '12345', 'asdfg', 'adhudcxmas'])
